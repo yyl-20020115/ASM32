eb2cbf2f9847549b64054aec54973a488e1e6914
杨铸人 <49382681@qq.com>
2025-12-02 12:01:38 +0800

    添加项目文件。

---
+++ ASM32MAIN.c
@@ -0,0 +1,13 @@
+﻿// ASM32.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <stdio.h>
+
+extern int __stdcall ASM32_ENTRY(int argc, char* file_path);
+
+int main(int argc, char* argv[])
+{
+	printf("Seattle Computer Products 8086 Assembler Version 2.44\n");
+	printf("Copyright 1979-1983 by Seattle Computer Products, Inc.\n");
+
+	return argc <= 1 ? -1 : ASM32_ENTRY(argc - 1, argv[1]);
+}
