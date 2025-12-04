b4e07d784ec0ffaf444a13c653cd2879f32c9470
杨铸人 <49382681@qq.com>
2025-12-04 09:31:23 +0800

    fx

---
+++ ASM32Symbols/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ASM32Symbols;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> lines = [];
+        List<string> labels = [];
+        using (var reader = new StreamReader("ASM32.ASM"))
+        {
+            string? line;
+            while (null != (line = reader.ReadLine()))
+            {
+                lines.Add(line);
+                var i = line.IndexOf(':');
+                if (i >= 0)
+                {
+                    var label = line[..i].Trim();
+                    if (label.All(c => char.IsAsciiLetterOrDigit(c)||c=='_'))
+                    {
+                        labels.Add(label);
+                    }
+                }
+            }
+        }
+        using (var writer = new StreamWriter("ASM32-G.ASM"))
+        {
+            foreach(var label in labels)
+            {
+                writer.WriteLine($"GLOBAL {label}");
+            }
+            foreach(var line in lines)
+            {
+                writer.WriteLine(line);
+            }
+        }
+    }
+}
